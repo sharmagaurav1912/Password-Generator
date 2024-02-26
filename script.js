5efd9f0c5b360172019299b6b684c3a1dc1a6526
Gaurav Sharma <145780214+sharmagaurav1912@users.noreply.github.com>
2024-02-26 10:07:22 +0530

    Update script.js
    //update the functionality for copy to clipboard and a message that text has been copied and copy image changed to tick
    //apply settimeout for message and image after 2 seconds

--- script.js
+++ script.js
@@ -22,5 +22,25 @@ function generatePassword() {
 
     passBox.value = password;
 
-}
+};
 
+let copyIcon = document.querySelector('#copy-icon');
+
+document.getElementById('copy-btn')
+.addEventListener("click", () => {
+
+    if (passBox.value) {
+        navigator.clipboard.writeText(passBox.value);
+
+        // passBox.setAttribute("Placeholder", "Text copied to Clipboard");
+
+        passBox.value = "Text copied to Clipboard";
+        copyIcon.src = 'img/check.png';
+
+        setTimeout(() => {
+            passBox.value = "";
+            copyIcon.src = 'img/R2ac5155460db535e7dc649ec9f5afdee.png';
+        },2000);
+    } 
+
+});
